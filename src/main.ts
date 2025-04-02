b18f5b112ef330a6fa58c4ef23ca39a5ad4c5ff1
unknown <diokolo@labelenergie.fr>
2025-04-02 11:54:29 +0200

    simulator

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['https://labelenergie.netlify.app', 'http://localhost:3000'],
+    origin: [
+      'https://diokolo.netlify.app',
+      'http://localhost:3000'  // pour le développement local
+    ],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
     credentials: true,
   });
   app.setGlobalPrefix('api');
